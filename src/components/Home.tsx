8f843f3f4bfecc4042d5ab1bf51c1d34d596cab4
Vivekanand P V <vivekanandpv@outlook.com>
2022-07-08 11:02:07 +0530

    done

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,18 @@
 import React, { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
+import { AppState } from '../store/app-store';
 
 interface HomeProps {}
 
 const Home: FunctionComponent<HomeProps> = (props: HomeProps) => {
+  const appState = useSelector<AppState, AppState>((state) => state);
+
   return (
     <>
       <h3>Home Component</h3>
+      <p>Albums: {appState.albums.length}</p>
+      <p>Users: {appState.users.length}</p>
+      <p>Todos: {appState.todos.length}</p>
     </>
   );
 };
