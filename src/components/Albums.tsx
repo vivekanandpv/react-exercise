675973184df1e0dea7531d5240853ec37c226c4b
Vivekanand P V <vivekanandpv@outlook.com>
2022-07-08 17:02:43 +0530

    done

--- src/components/Albums.tsx
+++ src/components/Albums.tsx
@@ -1,11 +1,33 @@
 import React, { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
+import { Album } from '../store/albums-slice';
+import { AppState } from '../store/app-store';
 
 interface AlbumsProps {}
 
 const Albums: FunctionComponent<AlbumsProps> = (props: AlbumsProps) => {
+  const albums = useSelector<AppState, Album[]>((state) => state.albums);
   return (
     <>
       <h3>Albums Component</h3>
+      <table className='table table-bordered table-sm'>
+        <thead>
+          <th>Id</th>
+          <th>Title</th>
+          <th>User Id</th>
+        </thead>
+        <tbody>
+          {albums.map((a) => {
+            return (
+              <tr key={a.id}>
+                <td>{a.id}</td>
+                <td>{a.title}</td>
+                <td>{a.userId}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </>
   );
 };
--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,11 +1,35 @@
 import React, { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
+import { User } from '../models/user.models';
+import { AppState } from '../store/app-store';
 
 interface UsersProps {}
 
 const Users: FunctionComponent<UsersProps> = (props: UsersProps) => {
+  const users = useSelector<AppState, User[]>((state) => state.users);
   return (
     <>
       <h3>Users Component</h3>
+      <table className='table table-bordered table-sm'>
+        <thead>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Email</th>
+        </thead>
+        <tbody>
+          {users.map((u) => {
+            return (
+              <tr key={u.id}>
+                <td>{u.id}</td>
+                <td>{u.name}</td>
+                <td>{u.username}</td>
+                <td>{u.email}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </>
   );
 };
